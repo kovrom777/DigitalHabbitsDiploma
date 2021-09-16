39aa120fb054199ddf426a4f5c021d8b4758931f
Roman Kovaykin <kovajkin2012@yandex.ru>
2021-09-16 23:54:31 +0300

    Инициализация проекта
    Инициализация проекта

---
+++ DiplomaDigitalHabbits/StartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  DiplomaDigitalHabbits
+//
+//  Created by Роман Ковайкин on 16.09.2021.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
